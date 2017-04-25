efeb10afa93b54e3961acd5738e4a8a9227977a2
Citrine <citrine1105@gmail.com>
2017-04-25 20:23:53 +0900

    マニフェスト追加 System DPI Aware

--- RamenTimer/Form1.Designer.cs
+++ RamenTimer/Form1.Designer.cs
@@ -108,6 +108,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "ラーメンタイマー";
