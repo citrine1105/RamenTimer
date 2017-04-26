3879ec7e6eae2898f50380f583a33d00a73f6cf8
Citrine <citrine1105@gmail.com>
2017-04-26 23:02:56 +0900

    画像を追加 表示を分秒形式に変更 時間設定機能を追加 ストップ機能を追加

--- RamenTimer/Form1.Designer.cs
+++ RamenTimer/Form1.Designer.cs
@@ -36,6 +36,11 @@
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,19 +56,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(235, 127);
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(218, 129);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 38);
+            this.label1.Size = new System.Drawing.Size(61, 33);
             this.label1.TabIndex = 1;
             this.label1.Text = "あと";
             // 
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("MS UI Gothic", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label2.Location = new System.Drawing.Point(313, 111);
+            this.label2.Location = new System.Drawing.Point(285, 109);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(108, 54);
+            this.label2.Size = new System.Drawing.Size(53, 54);
             this.label2.TabIndex = 2;
             this.label2.Text = "0";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -71,19 +76,19 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label3.Location = new System.Drawing.Point(427, 127);
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(344, 129);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 38);
+            this.label3.Size = new System.Drawing.Size(47, 33);
             this.label3.TabIndex = 3;
-            this.label3.Text = "秒";
+            this.label3.Text = "分";
             // 
             // progressBar1
             // 
             this.progressBar1.Location = new System.Drawing.Point(218, 12);
             this.progressBar1.Maximum = 360;
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(264, 23);
+            this.progressBar1.Size = new System.Drawing.Size(312, 23);
             this.progressBar1.TabIndex = 4;
             // 
             // timer1
@@ -94,7 +99,7 @@
             // button1
             // 
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.button1.Location = new System.Drawing.Point(407, 41);
+            this.button1.Location = new System.Drawing.Point(455, 41);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
@@ -102,11 +107,63 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(218, 46);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "設定時間";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(277, 43);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(30, 19);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(313, 46);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 12);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "分";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(397, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 54);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "0";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(483, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 33);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "秒";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(494, 174);
+            this.ClientSize = new System.Drawing.Size(542, 174);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label3);
@@ -133,6 +190,11 @@
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- RamenTimer/Form1.cs
+++ RamenTimer/Form1.cs
@@ -15,29 +15,76 @@ namespace RamenTimer
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Image = Image.FromFile("./images/1_1.png");
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            label7.Font = SystemFonts.MessageBoxFont;
+            label8.Font = SystemFonts.MessageBoxFont;
             button1.Font = SystemFonts.MessageBoxFont;
+            textBox1.Text = "3";
+            textBox1.Font = SystemFonts.MessageBoxFont;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            progressBar1.Value = 0;
-            int starttime = progressBar1.Maximum / 2;
-            label2.Text = starttime.ToString();
-            timer1.Enabled = true;
+            if (timer1.Enabled)
+            {
+                progressBar1.Value = 0;
+                label2.Text = "0";
+                label4.Text = "0";
+                timer1.Enabled = false;
+                textBox1.Enabled = true;
+                button1.Text = "スタート";
+            }
+            else
+            {
+                progressBar1.Value = 0;
+                progressBar1.Maximum = int.Parse(textBox1.Text) * 60 * 2;   // 時間設定
+                int starttime = progressBar1.Maximum / 2;
+                label2.Text = string.Format("{0}", starttime / 60);
+                label4.Text = string.Format("{0}", starttime % 60);
+                timer1.Enabled = true;
+                textBox1.Enabled = false;
+                button1.Text = "ストップ";
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             progressBar1.Value++;
-            label2.Text = Convert.ToString((progressBar1.Maximum  - progressBar1.Value) / 2);
+            label2.Text = Convert.ToString((progressBar1.Maximum  - progressBar1.Value) / 2 / 60);
+            label4.Text = Convert.ToString((progressBar1.Maximum - progressBar1.Value) / 2 % 60);
+            if (progressBar1.Maximum - progressBar1.Value < 120)    // 残り時間が1分以内の場合
+            {
+                if (progressBar1.Value % 2 == 0)
+                {
+                    pictureBox1.Image = Image.FromFile("./images/2_1.png");
+                }
+                else
+                {
+                    pictureBox1.Image = Image.FromFile("./images/2_2.png");
+                }
+            }
+            else
+            {
+                if (progressBar1.Value % 2 == 0)
+                {
+                    pictureBox1.Image = Image.FromFile("./images/1_1.png");
+                }
+                else
+                {
+                    pictureBox1.Image = Image.FromFile("./images/1_2.png");
+                }
+            }
             if (progressBar1.Value == progressBar1.Maximum)
             {
                 timer1.Enabled = false;
+                pictureBox1.Image = Image.FromFile("./images/3_1.png");
                 MessageBox.Show("いただきます", "ラーメンタイマー", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBox1.Enabled = true;
+                button1.Text = "スタート";
             }
         }
     }
